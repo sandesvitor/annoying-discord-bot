6c4eddac2744697c3aa37a7aa82ff8428a0c67a4
Vitor Sandes <sandesvitor@gmail.com>
2020-12-17 22:59:16 -0300

    Added game funtionality

--- main.py
+++ main.py
@@ -1,37 +1,122 @@
 import discord
+from discord.ext import commands, tasks
+from itertools import cycle
+import random
 import os
 import requests
-# import json
 
-client = discord.Client()
+
+client = commands.Bot(command_prefix='--')
+status = cycle(
+    [
+        'Jogo da Vida', 
+        'Catanzinho', 
+        'Amongão Suave',
+        'FF7 - Remake',
+        'Residento Mau',
+        'Churrasco',
+        'Biriba'
+    ]
+)
+
+coins = 0
+
+
+
+###################################
+###        MAIN FUNCTIONS       ###
+###################################
 
 def get_random_insult():
-    response = requests.get(r"https://insult.mattbas.org/api/insult")
+    response = requests.get(
+        r"https://insult.mattbas.org/api/insult")
     return response.text
 
 def post_annoying_song():
     pass
 
 
+###################################
+###          BOT EVENTS         ###
+###################################
+
 @client.event
 async def on_ready():
+    change_status.start()
     print("Logged in as {0}".format(client.user))
 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+async def on_member_join(member):
+    print(f"{member} has join the server.")
 
-    if message.content.startswith("--help"):
-        await message.channel.send(
-            "Eu sou o Vinny Babaquetti! Hoje vocês terão o prazer de interagir com minha pessoa. Meus comandos mágicos são:\n" +
-            "\n" +
-            "1) '--help' : para o qual repetirás esta mesma merda!\n" +
-            "2) '--inspira' : ofereço-lhe meus humildes dois centavos sobre vossa pessoa!\n"
-        )
+@client.event
+async def on_member_remove(member):
+    print(f"{member} has left the server.")
 
 
-    if message.content.startswith("--inspira"):
-        await message.channel.send(get_random_insult())
+###################################
+###          BOT TASKS          ###
+###################################
+
+@tasks.loop(seconds=random.randint(60, 600))
+async def change_status():
+    await client.change_presence(activity=discord.Game(next(status)))
+
+
+###################################
+###         BOT COMMANDS        ###
+###################################
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Latência: {round(client.latency * 1000)}ms")
+
+@client.command()
+async def inspirar(ctx):
+    await ctx.send(get_random_insult())
+
+@client.command()
+@commands.has_permissions(manage_messages=True)
+async def apagar(ctx, amount : int):
+    await ctx.channel.purge(limit=amount)
+
+@client.command()
+async def moeda(ctx):
+    global coins
+    coins += 1
+    isPlural = "moedas" if coins > 1 else "moeda" 
+
+    await ctx.send(
+        f"Opa! Uma moedinha!\n" + 
+        "#################################################\n" +
+        f"# Vinícius está com {coins} {isPlural}\n" +
+        "#################################################\n"
+    )
+
+@client.command()
+async def roubar(ctx):
+    global coins
+    coins -= 1
+    coins = 0 if coins <= 0 else coins
+    isPlural = "moedas" if coins > 1 else "moeda" 
+    prompt = f"Por que me roubastes!?" if coins > 0 else "Minha carteira já tá vazia, merda..."
+
+    await ctx.send(
+        f"{prompt}\n" + 
+        "#################################################\n" +
+        f"# Vinícius está com {coins} {isPlural}\n" +
+        "#################################################\n"
+    )
+
+###################################
+###        COMMAND ERRORS       ###
+###################################
+
+@apagar.error
+async def apagar_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Faltou especificar quantas linhas tu quer apagar, porra!")
+
+
 
 client.run(os.getenv("TOKEN"))
